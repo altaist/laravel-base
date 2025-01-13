ce6ef559da754d8b86069b5e27a0eed455af22e5
Alexander Pecherskiy <apecherskiy@yandex.ru>
2025-01-13 23:12:25 +0700

    components and plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,12 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import {ProjectPlugin} from '@/plugins/project';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+window.TWA = window.Telegram ? window.Telegram.WebApp : null;
+window.debug = (...t) => console.log(...t);
+window.redirect =  (path) => window.location = path;
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -16,10 +20,13 @@ createInertiaApp({
             import.meta.glob('./pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el);
+            .use(ProjectPlugin);
+
+        return app.mount(el);
+
     },
     progress: {
         color: '#4B5563',
---
+++ resources/js/plugins/project.js
@@ -0,0 +1,35 @@
+/*
+import PageLayout from '@/components/shared/PageLayout.vue';
+import PageHeader from '@/components/shared/PageHeader.vue';
+import PageFooter from '@/components/shared/PageFooter.vue';
+import PageSection from '@/components/shared/PageSection.vue';
+import SectionTitle from '@/components/shared/SectionTitle.vue';
+import PageContainer from '@/components/shared/PageContainer.vue';
+import PageTitle from '@/components/shared/PageTitle.vue';
+import Btn from '@/components/shared/Btn.vue';
+import Block from '@/components/shared/Block.vue';
+*/
+
+export const ProjectPlugin = {
+    install: (app, options) => {
+
+/*
+        app.component("layout", PageLayout);
+        app.component("page-header", PageHeader);
+        app.component("page-title", PageTitle);
+        app.component("page-footer", PageFooter);
+        app.component("page-section", PageSection);
+        app.component("page-section-title", SectionTitle);
+        app.component("block", Block);
+        app.component("btn", Btn);
+
+        app.component("btn-flat", BtnFlat);
+        app.component("btn-ok", BtnOk);
+        app.component("btn-cancel", BtnCancel);
+        app.component("btn-profile", BtnProfile);
+        app.component("btn-edit", BtnEdit);
+        app.component("btn-back", BtnBack);
+*/
+    }
+}
+
